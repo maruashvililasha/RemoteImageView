b94dfbb30c0fd9a90f49946c24d9ee9f67f123ef
Lasha Maruashvili <38511844+maruashvililasha@users.noreply.github.com>
2021-12-07 20:11:58 +0400

    purge

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -1,30 +1,35 @@
 import SwiftUI
 
 public class RemoteImageFetcher: ObservableObject {
-  @Published var imageData = Data()
-  let url: URL
-
-  public init(url: URL) {
-    self.url = url
-  }
-
-  // 1
-  public func fetch() {
-    URLSession.shared.dataTask(with: url) { (data, _, _) in
-      guard let data = data else { return }
-      DispatchQueue.main.async {
-        self.imageData = data
-      }
-    }.resume()
-  }
-
-  // 2
-  public func getImageData() -> Data {
-    return imageData
-  }
-
-  // 3
-  public func getUrl() -> URL {
-    return url
-  }
+    @Published var imageData = Data()
+    let url: URL
+    
+    public init(url: URL) {
+        self.url = url
+    }
+    
+    public func purge() {
+        imageData = Data()
+    }
+    
+    
+    // 1
+    public func fetch() {
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.imageData = data
+            }
+        }.resume()
+    }
+    
+    // 2
+    public func getImageData() -> Data {
+        return imageData
+    }
+    
+    // 3
+    public func getUrl() -> URL {
+        return url
+    }
 }
